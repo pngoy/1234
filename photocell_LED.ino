0e235f957d3d1e4957e0a9cdf646e298c8e2a9b2
pngoy <72630979+pngoy@users.noreply.github.com>
2020-11-12 21:02:25 -0800

    Update photocell_LED.ino
    Added some comments and the 5 Min timer.

--- photocell_LED.ino
+++ photocell_LED.ino
@@ -1,50 +1,53 @@
-//https://create.arduino.cc/projecthub/55546/innovation-lab-7-photocell-led-on-off-3df0ab
-//11.12.2020
+//Bird feeder
 
-//www.elegoo.com
-//2016.12.08
+//When bird lands on the food trey.
+//The Micro processor will detect the bird shadow via the Photo senor.
+//The Micro processor will Turn the servo 180 degrees clockwise opening the seed latch
+//The Micro processor will Turn the servo 180 degrees counter-clockwise closing the seed latch
+//The Micro processor will Wait for about 5 minute before it can sense another shadow.
+//So, that the same bird does get feed more than once.
 
 #include <Servo.h>
 
-const int sensorPin = 0;
-const int ledPin = 9;
-int lightCal;
-int lightVal;
+const int sensorPin = 0;                //
+const int ledPin = 9;                   //
+int lightCal;                           // Intialize the Photo resistor 
+int lightVal;                           // Intialize the Photo resistor Value
 
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo myservo;                          // create servo object to control a servo
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;                            // variable to store the servo position
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   lightCal = analogRead(sensorPin);
-  Serial.begin(9600);
-  myservo.attach(8);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);                   // Read out serial  
+  myservo.attach(8);                    // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  lightVal = analogRead(sensorPin);
+  lightVal = analogRead(sensorPin);     // Light sensor 
 
   // the photocell detect
-  if (lightVal < lightCal - 30) {
-    digitalWrite(ledPin, HIGH);  
+  if (lightVal < lightCal - 30) {       // compare the value of the light sensor 
+    digitalWrite(ledPin, HIGH);         // output to start the servo moto
    
-    delay(1000);
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    delay(250);
+  for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+    delay(20);                          // waits 15ms for the servo to reach the position
   }
   
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 180; pos >= 0; pos -= 5) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+    delay(20);                          // waits 1 sec for the servo to reach the position
   }
+  delay(300000);                        // wait for appromiatly 5 min after the switch open and close
   }
   
   else {
-    digitalWrite (ledPin, LOW);
+    digitalWrite (ledPin, LOW);         // else if the led is low
   }
 }
